0a97fbdc67ab441003adc89db2ec92e844444b01
e01 <dimitrov.adrian@gmail.com>
2021-01-12 10:39:43 +0200

    use editorconfig from directus

--- src/index.js
+++ src/index.js
@@ -1,105 +1,105 @@
-import Interface from "./interface.vue";
+import Interface from './interface.vue';
 
 const i18n = {
-  t: (s) => s,
-}
+	t: (s) => s,
+};
 
 export default {
-  id: "extension-masked",
-  name: "Masked Input",
-  description: "Masked input field",
-  icon: "edit",
-  component: Interface,
-  types: ["string"],
-  options: [
-    {
-      field: 'placeholder',
-      name: i18n.t('placeholder'),
-      meta: {
-        width: 'half',
-        interface: 'text-input',
-        options: {
-          placeholder: i18n.t('enter_a_placeholder'),
-        },
-      },
-    },
-    {
-      field: 'font',
-      name: i18n.t('font'),
-      type: 'string',
-      meta: {
-        width: 'half',
-        interface: 'dropdown',
-        options: {
-          choices: [
-            { text: i18n.t('sans_serif'), value: 'sans-serif' },
-            { text: i18n.t('monospace'), value: 'monospace' },
-            { text: i18n.t('serif'), value: 'serif' },
-          ],
-        },
-      },
-      schema: {
-        default_value: 'sans-serif',
-      },
-    },
-    {
-      field: 'iconLeft',
-      name: i18n.t('icon_left'),
-      type: 'string',
-      meta: {
-        width: 'half',
-        interface: 'icon',
-      },
-    },
-    {
-      field: 'iconRight',
-      name: i18n.t('icon_right'),
-      type: 'string',
-      meta: {
-        width: 'half',
-        interface: 'icon',
-      },
-    },
-    {
-      field: 'storeMasked',
-      name: i18n.t('interfaces.datetime.include_seconds'),
-      type: 'boolean',
-      meta: {
-        width: 'half',
-        interface: 'toggle',
-      },
-      schema: {
-        default_value: false,
-      },
-    },
-    {
-      field: 'transform',
-      name: i18n.t('transform'),
-      type: 'string',
-      meta: {
-        width: 'half',
-        interface: 'dropdown',
-        options: {
-          choices: [
-            { text: i18n.t('No'), value: '' },
-            { text: i18n.t('Uppercase'), value: 'uppercase' },
-            { text: i18n.t('Lowercase'), value: 'lowercase' },
-          ],
-        },
-      },
-      schema: {
-        default_value: '',
-      },
-    },
-    {
-      field: 'help',
-      name: i18n.t('Help'),
-      type: 'alias',
-      meta: {
-        width: 'full',
-        interface: 'notice',
-        options: {
-          text: `
+	id: 'extension-masked',
+	name: 'Masked Input',
+	description: 'Masked input field',
+	icon: 'crop_7_5',
+	component: Interface,
+	types: ['string'],
+	options: [
+		{
+			field: 'placeholder',
+			name: i18n.t('placeholder'),
+			meta: {
+				width: 'half',
+				interface: 'text-input',
+				options: {
+					placeholder: i18n.t('enter_a_placeholder'),
+				},
+			},
+		},
+		{
+			field: 'font',
+			name: i18n.t('font'),
+			type: 'string',
+			meta: {
+				width: 'half',
+				interface: 'dropdown',
+				options: {
+					choices: [
+						{text: i18n.t('sans_serif'), value: 'sans-serif'},
+						{text: i18n.t('monospace'), value: 'monospace'},
+						{text: i18n.t('serif'), value: 'serif'},
+					],
+				},
+			},
+			schema: {
+				default_value: 'sans-serif',
+			},
+		},
+		{
+			field: 'iconLeft',
+			name: i18n.t('icon_left'),
+			type: 'string',
+			meta: {
+				width: 'half',
+				interface: 'icon',
+			},
+		},
+		{
+			field: 'iconRight',
+			name: i18n.t('icon_right'),
+			type: 'string',
+			meta: {
+				width: 'half',
+				interface: 'icon',
+			},
+		},
+		{
+			field: 'storeMasked',
+			name: i18n.t('interfaces.datetime.include_seconds'),
+			type: 'boolean',
+			meta: {
+				width: 'half',
+				interface: 'toggle',
+			},
+			schema: {
+				default_value: false,
+			},
+		},
+		{
+			field: 'transform',
+			name: i18n.t('transform'),
+			type: 'string',
+			meta: {
+				width: 'half',
+				interface: 'dropdown',
+				options: {
+					choices: [
+						{text: i18n.t('No'), value: ''},
+						{text: i18n.t('Uppercase'), value: 'uppercase'},
+						{text: i18n.t('Lowercase'), value: 'lowercase'},
+					],
+				},
+			},
+			schema: {
+				default_value: '',
+			},
+		},
+		{
+			field: 'help',
+			name: i18n.t('Help'),
+			type: 'alias',
+			meta: {
+				width: 'full',
+				interface: 'notice',
+				options: {
+					text: `
             0 - any digit
             a - any letter
             * - any char
@@ -108,54 +108,54 @@ export default {
             {} - include fixed part in unmasked value
             \` - prevent symbols shift back
           `,
-          icon: 'share',
-        },
-      },
-    },
-    {
-      field: 'masks',
-      name: i18n.t('interfaces.color.preset_colors'),
-      type: 'string',
-      meta: {
-        width: 'full',
-        interface: 'repeater',
-        options: {
-          addLabel: i18n.t('interfaces.color.preset_colors_add_label'),
-          template: '{{ mask }}',
-          fields: [
-            {
-              field: 'type',
-              name: i18n.t('type'),
-              type: 'string',
-              meta: {
-                width: 'half',
-                interface: 'dropdown',
-                options: {
-                  choices: [
-                    { text: i18n.t('simple'), value: '' },
-                    { text: i18n.t('regex'), value: 'regex' },
-                  ],
-                },
-              },
-              schema: {
-                default_value: '',
-              },
-            },
-            {
-              field: 'mask',
-              type: 'string',
-              name: i18n.t('name'),
-              meta: {
-                interface: 'text-input',
-                width: 'half',
-                options: {
-                  placeholder: i18n.t('interfaces.color.name_placeholder'),
-                },
-              },
-            },
-          ],
-        },
-      },
-    },
-  ],
+					icon: 'share',
+				},
+			},
+		},
+		{
+			field: 'masks',
+			name: i18n.t('interfaces.color.preset_colors'),
+			type: 'string',
+			meta: {
+				width: 'full',
+				interface: 'repeater',
+				options: {
+					addLabel: i18n.t('interfaces.color.preset_colors_add_label'),
+					template: '{{ mask }}',
+					fields: [
+						{
+							field: 'type',
+							name: i18n.t('type'),
+							type: 'string',
+							meta: {
+								width: 'half',
+								interface: 'dropdown',
+								options: {
+									choices: [
+										{text: i18n.t('simple'), value: ''},
+										{text: i18n.t('regex'), value: 'regex'},
+									],
+								},
+							},
+							schema: {
+								default_value: '',
+							},
+						},
+						{
+							field: 'mask',
+							type: 'string',
+							name: i18n.t('name'),
+							meta: {
+								interface: 'text-input',
+								width: 'half',
+								options: {
+									placeholder: i18n.t('interfaces.color.name_placeholder'),
+								},
+							},
+						},
+					],
+				},
+			},
+		},
+	],
 };
--- src/interface.vue
+++ src/interface.vue
@@ -1,138 +1,139 @@
 <template>
-  <v-input
-      :value="masker.value"
-      :placeholder="placeholder"
-      :disabled="disabled"
-      :class="className"
-      @input="handleInput"
-  >
-    <template v-if="iconLeft" #prepend>
-      <v-icon :name="iconLeft"/>
-    </template>
-    <template v-if="iconRight" #append>
-      <v-icon :name="iconRight"/>
-    </template>
-  </v-input>
+	<v-input
+			:value="masker.value"
+			:placeholder="placeholder"
+			:disabled="disabled"
+			:class="className"
+			@input="handleInput"
+	>
+		<template v-if="iconLeft" #prepend>
+			<v-icon :name="iconLeft"/>
+		</template>
+		<template v-if="iconRight" #append>
+			<v-icon :name="iconRight"/>
+		</template>
+	</v-input>
 </template>
 
 <script>
-import IMask from 'imask'
+import IMask from 'imask';
 
 export default {
-  props: {
-    value: {
-      type: String,
-      default: null,
-    },
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-    placeholder: {
-      type: String,
-      default: null,
-    },
-    iconLeft: {
-      type: String,
-      default: null,
-    },
-    iconRight: {
-      type: String,
-      default: null,
-    },
-    font: {
-      type: String,
-      default: 'sans-serif',
-    },
-    storeMasked: {
-      type: Boolean,
-      default: true,
-    },
-    transform: {
-      type: String,
-      default: '',
-    },
-    masks: {
-      type: Array,
-      default: () => [],
-    },
+	props: {
+		value: {
+			type: String,
+			default: null,
+		},
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
+		placeholder: {
+			type: String,
+			default: null,
+		},
+		iconLeft: {
+			type: String,
+			default: null,
+		},
+		iconRight: {
+			type: String,
+			default: null,
+		},
+		font: {
+			type: String,
+			default: 'sans-serif',
+		},
+		storeMasked: {
+			type: Boolean,
+			default: true,
+		},
+		transform: {
+			type: String,
+			default: '',
+		},
+		masks: {
+			type: Array,
+			default: () => [],
+		},
 
-  },
+	},
 
-  methods: {
-    handleInput: function (value) {
-      this.masker.resolve(value)
-      let finalValue = this.$props.storeMasked ? this.masker.value : this.masker.unmaskedValue;
-      if (this.$props.transform === 'uppercase') {
-        finalValue = finalValue.toUpperCase()
-      } else if (this.$props.transform === 'lowercase') {
-        finalValue = finalValue.toLowerCase()
-      }
-      this.$emit('input', finalValue)
-    },
-    buildMaskConfig: function () {
-      return {
-        mask: this.$props.masks.map((item) => {
-          let pattern = item.mask
-          if (item.type === 'regex') {
-            try {
-              pattern = new RegExp(item.mask)
-            }
-            catch (error) {
-              return null
-            }
-          }
-          return {
-            mask: pattern,
-          }
-        }).filter((i) => !!i),
-      }
-    },
-  },
+	methods: {
+		handleInput: function(value) {
+			this.masker.resolve(value);
+			let finalValue = this.$props.storeMasked ? this.masker.value : this.masker.unmaskedValue;
+			if (this.$props.transform === 'uppercase') {
+				finalValue = finalValue.toUpperCase();
+			} else {
+				if (this.$props.transform === 'lowercase') {
+					finalValue = finalValue.toLowerCase();
+				}
+			}
+			this.$emit('input', finalValue);
+		},
+		buildMaskConfig: function() {
+			return {
+				mask: this.$props.masks.map((item) => {
+					let pattern = item.mask;
+					if (item.type === 'regex') {
+						try {
+							pattern = new RegExp(item.mask);
+						} catch (error) {
+							return null;
+						}
+					}
+					return {
+						mask: pattern,
+					};
+				}).filter((i) => !!i),
+			};
+		},
+	},
 
-  watch: {
-    value: function(newValue, oldValue) {
-      if (oldValue === null) {
-        this.masker.resolve(newValue)
-      }
-    }
-  },
+	watch: {
+		value: function(newValue, oldValue) {
+			if (oldValue === null) {
+				this.masker.resolve(newValue);
+			}
+		},
+	},
 
-  computed: {
-    className: function() {
-      return {
-        [this.$props.font]: true,
-        invalid: this.value && !this.masker.isComplete,
-      }
-    },
-  },
+	computed: {
+		className: function() {
+			return {
+				[this.$props.font]: true,
+				invalid: this.value && !this.masker.isComplete,
+			};
+		},
+	},
 
-  data: function () {
+	data: function() {
 
-    window.hh = this
-    window.IMask = IMask;
-    return {
-      masker: IMask.createMask(this.buildMaskConfig())
-    }
-  },
+		window.hh = this;
+		window.IMask = IMask;
+		return {
+			masker: IMask.createMask(this.buildMaskConfig()),
+		};
+	},
 
-}
+};
 </script>
 
 <style lang="css" scoped>
 .monospace {
-  --v-input-font-family: var(--family-monospace);
+	--v-input-font-family: var(--family-monospace);
 }
 
 .serif {
-  --v-input-font-family: var(--family-serif);
+	--v-input-font-family: var(--family-serif);
 }
 
 .sans-serif {
-  --v-input-font-family: var(--family-sans-serif);
+	--v-input-font-family: var(--family-sans-serif);
 }
 
 .invalid >>> .input {
-  border-color: var(--warning) !important;
+	border-color: var(--warning) !important;
 }
 </style>
