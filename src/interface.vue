f9e5c6d23c5d19e7dfdd70bce70aeeabf0d01010
e01 <dimitrov.adrian@gmail.com>
2022-05-05 16:17:09 +0300

    Use custom input element in place of v-input to deal with the value update

--- src/interface.vue
+++ src/interface.vue
@@ -1,22 +1,23 @@
 <template>
-	<v-input
-		ref="inputElement"
-		:model-value="value"
-		:placeholder="placeholder"
-		:disabled="disabled"
-		:class="{ [font]: font }"
-	>
-		<template v-if="iconLeft" #prepend>
-			<v-icon :name="iconLeft" />
-		</template>
-		<template v-if="iconRight" #append>
-			<v-icon :name="iconRight" />
-		</template>
-	</v-input>
+	<div class="masked-interface" :disabled="disabled" :class="{ [font]: font }">
+		<div class="input" :class="{ invalid, disabled, active }">
+			<template v-if="iconLeft">
+				<span class="prefix">
+					<v-icon :name="iconLeft" />
+				</span>
+			</template>
+			<input ref="input" :disabled="disabled" :placeholder="placeholder" @input="onInput" />
+			<template v-if="iconRight">
+				<span class="suffix">
+					<v-icon :name="iconRight" />
+				</span>
+			</template>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted, onUnmounted, PropType } from 'vue';
+import { defineComponent, ref, onMounted, onUnmounted, PropType, watch } from 'vue';
 import Inputmask from 'inputmask';
 
 export default defineComponent({
@@ -25,7 +26,13 @@ export default defineComponent({
 			type: String,
 			default: null,
 		},
+		required: {
+			type: Boolean,
+		},
 		disabled: {
+			type: Boolean,
+		},
+		active: {
 			type: Boolean,
 		},
 		placeholder: {
@@ -63,17 +70,18 @@ export default defineComponent({
 	},
 	emits: ['input'],
 	setup(props, { emit }) {
-		const inputElement = ref<{ input: HTMLInputElement } | null>(null);
+		const input = ref<HTMLInputElement>(null);
 		const inputMaskInstance = ref<Inputmask.Instance | null>(null);
+		const invalid = ref<boolean>(false);
+		const aliasType = ['regex', 'mask'].includes(props.templateType) ? null : props.templateType;
+
+		const customArgs: Inputmask.Options = {};
+		if (['regex', 'mask'].includes(props.templateType)) {
+			customArgs[props.templateType] = props.template || '';
+		}
 
 		onMounted(() => {
-			const aliasType = ['regex', 'mask'].includes(props.templateType) ? null : props.templateType;
-
-			const customArgs: Inputmask.Options = {};
-
-			if (['regex', 'mask'].includes(props.templateType)) {
-				customArgs[props.templateType] = props.template || '';
-			}
+			if (!input.value) return;
 
 			inputMaskInstance.value = Inputmask(aliasType, {
 				...customArgs,
@@ -86,54 +94,185 @@ export default defineComponent({
 				clearIncomplete: false,
 				tabThrough: true,
 				nullable: true,
+				oncleared: onClear,
+				// Build-in event hooks (onincomplete, oncomplete, onclear) are not sufficient do good emit.
+			}).mask(input.value);
 
-				onincomplete: () => {
-					inputElement.value.input.classList.add('invalid');
-				},
-
-				onKeyValidation: () => {
-					inputElement.value.input.classList.toggle('invalid', !inputMaskInstance.value.isValid());
-				},
-
-				oncomplete: () => {
-					inputElement.value.input.classList.remove('invalid');
-
-					if (props.storeMasked) {
-						emit('input', inputMaskInstance.value.format(inputMaskInstance.value.unmaskedvalue()));
-					} else {
-						emit('input', inputMaskInstance.value.unmaskedvalue());
-					}
-				},
-			}).mask(inputElement.value.input);
+			inputMaskInstance.value.setValue(props.value || '');
+			invalid.value = !isValid();
 		});
+
+		watch(
+			() => props.value,
+			(newValue) => {
+				inputMaskInstance.value.setValue(newValue || '');
+				invalid.value = !isValid();
+			}
+		);
 
 		onUnmounted(() => {
 			if (!inputMaskInstance.value) return;
-
 			inputMaskInstance.value.remove();
 		});
 
 		return {
-			inputElement,
+			input,
+			onInput,
+			invalid,
 		};
+
+		function onClear() {
+			invalid.value = false;
+			emitValue(null);
+		}
+
+		function onComplete() {
+			invalid.value = false;
+			emitValue(getValue());
+		}
+
+		function onIncomplete() {
+			invalid.value = true;
+			emitValue(props.value);
+		}
+
+		function onInput() {
+			if (inputMaskInstance.value.isComplete()) {
+				onComplete();
+			} else {
+				onIncomplete();
+			}
+		}
+
+		function isValid() {
+			if (props.required && !getValue()) return false;
+
+			return inputMaskInstance.value.isComplete();
+		}
+
+		function emitValue(value: string | null) {
+			if (props.disabled) return;
+			if (value === props.value) return;
+
+			if (value && inputMaskInstance.value.isValid(value)) {
+				emit('input', value);
+			} else {
+				emit('input', null);
+			}
+		}
+
+		function getValue() {
+			const unmaskedvalue = inputMaskInstance.value.unmaskedvalue();
+			if (!unmaskedvalue) return null;
+
+			if (props.storeMasked) return inputMaskInstance.value.format(unmaskedvalue);
+			return unmaskedvalue;
+		}
 	},
 });
 </script>
 
-<style lang="css" scoped>
-.monospace {
-	--v-input-font-family: var(--family-monospace);
-}
+<style lang="scss" scoped>
+.masked-interface {
+	--arrow-color: var(--border-normal);
+	--v-icon-color: var(--foreground-subdued);
+	--v-input-color: var(--foreground-normal);
+	--v-input-background-color: var(--background-input);
+	--v-input-border-color-focus: var(--primary);
+	height: var(--input-height);
 
-.serif {
-	--v-input-font-family: var(--family-serif);
-}
+	&.monospace {
+		--v-input-font-family: var(--family-monospace);
+	}
 
-.sans-serif {
-	--v-input-font-family: var(--family-sans-serif);
-}
+	&.serif {
+		--v-input-font-family: var(--family-serif);
+	}
 
-:deep(input.invalid) {
-	color: var(--danger);
+	&.sans-serif {
+		--v-input-font-family: var(--family-sans-serif);
+	}
+
+	.input {
+		position: relative;
+		display: flex;
+		flex-grow: 1;
+		align-items: center;
+		height: 100%;
+		padding: var(--input-padding);
+		padding-top: 0px;
+		padding-bottom: 0px;
+		color: var(--v-input-color);
+		font-family: var(--v-input-font-family);
+		background-color: var(--v-input-background-color);
+		border: var(--border-width) solid var(--border-normal);
+		border-radius: var(--border-radius);
+		transition: border-color var(--fast) var(--transition);
+
+		&:hover {
+			--arrow-color: var(--border-normal-alt);
+			color: var(--v-input-color);
+			background-color: var(--background-input);
+			border-color: var(--border-normal-alt);
+		}
+		&:focus-within,
+		&.active {
+			--arrow-color: var(--border-normal-alt);
+			color: var(--v-input-color);
+			background-color: var(--background-input);
+			border-color: var(--v-input-border-color-focus);
+			box-shadow: 0 0 16px -8px var(--primary);
+		}
+		&.disabled {
+			--arrow-color: var(--border-normal);
+			color: var(--foreground-subdued);
+			background-color: var(--background-subdued);
+			border-color: var(--border-normal);
+		}
+		.prefix,
+		.suffix {
+			color: var(--foreground-subdued);
+		}
+		.prefix {
+			margin-right: 8px;
+		}
+	}
+
+	input {
+		flex-grow: 1;
+		width: 20px; /* allows flex to grow/shrink to allow for slots */
+		height: 100%;
+		padding: var(--input-padding);
+		padding-right: 0px;
+		padding-left: 0px;
+		font-family: var(--v-input-font-family);
+		background-color: transparent;
+		border: none;
+		appearance: none;
+		&::placeholder {
+			color: var(--v-input-placeholder-color);
+		}
+		&:focus {
+			border-color: var(--v-input-border-color-focus);
+		}
+	}
+
+	.input.invalid input {
+		color: var(--danger);
+	}
+
+	&.small {
+		height: 38px;
+		.input {
+			padding: 8px 12px;
+		}
+	}
+
+	&.full-width {
+		width: 100%;
+		.input {
+			width: 100%;
+		}
+	}
 }
 </style>
